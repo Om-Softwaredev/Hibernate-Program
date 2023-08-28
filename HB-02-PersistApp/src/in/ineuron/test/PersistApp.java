31514f6f412bf4766ce2fc52385674dc0dddfbe0
Omprakash <omprakash8578502@gmail.com>
2023-08-28 09:55:09 +0530

    save()/persist()

---
+++ HB-02-PersistApp/src/in/ineuron/test/PersistApp.java
@@ -0,0 +1,52 @@
+package in.ineuron.test;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import in.ineuron.model.Student;
+import in.ineuron.util.HibernateUtil;
+
+public class PersistApp {
+
+	public static void main(String[] args) {
+		Session session = null;
+		Transaction transaction = null;
+		boolean flag = false;
+		Integer id = null;
+
+		try {
+			session = HibernateUtil.getSession();
+
+			if (session != null)
+				transaction = session.beginTransaction();
+
+			if (transaction != null) {
+				Student student = new Student();
+				student.setSid(10);
+				student.setSname("Sachin");
+				student.setSaddress("MI");
+
+//				id = (Integer) session.save(student);
+				session.persist(student);
+				flag = true;
+			}
+
+		} catch (HibernateException e) {
+
+		} catch (Exception e) {
+			// TODO: handle exception
+		} finally {
+			if (flag) {
+				transaction.commit();
+				System.out.println("Object saved to database.....");
+			} else {
+				transaction.rollback();
+				System.out.println("Object not saved to database.....");
+			}
+			HibernateUtil.closeSession(session);
+			HibernateUtil.closeSessionFactory();
+		}
+	}
+
+}
